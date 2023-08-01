81451d560cb0eb18531b35b6356442692cf3401f
Rubén Prieto Serrano <131255999+Rubpri@users.noreply.github.com>
2023-08-01 17:14:40 +0000

    final2

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -7,7 +7,7 @@ export const Home = () => {
 
 	<div className="video-container">
         <video autoPlay loop muted className="video-background">
-        <source src="https://vod-progressive.akamaized.net/exp=1690411107~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F536%2F6%2F152682484%2F469368836.mp4~hmac=e81f498fbcc6d7e3ba52206a2e035d3b66f8706d9bfb9195a25e662072e15c66/vimeo-prod-skyfire-std-us/01/536/6/152682484/469368836.mp4?filename=file.mp4" type="video/mp4" />
+        <source src="https://cdn.pixabay.com/vimeo/602894784/astronave-87995.mp4?width=640&hash=735a1e6db188cb2037c05eaca41398fdb3870306" type="video/mp4" />
         </video>
     </div>
     
